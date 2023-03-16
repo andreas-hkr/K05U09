9d677e8e93b4dbab3f6af06f7ad73133fabf478d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 07:37:53 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Beräkna simhoppspoäng
@@ -13,6 +15,39 @@ public class Main {
         //
         // Fortsätt tills svårighetsgraden är negativ
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange antal domare: ");
+        int ref = input.nextInt();
 
+        if (ref < 3) {
+            System.out.println("Felaktigt antal domare (minst 3)");
+            System.exit(0);
+        }
+
+        while (true) {
+            System.out.print("Ange svårighetsgrad: ");
+            double level = input.nextDouble();
+            if (level <= 0) {
+                break;
+            }
+
+            double min = Double.MAX_VALUE;
+            double max = 0;
+            double sum = 0;
+            for (int i=0; i < ref; i++) {
+                System.out.print("Ange poäng för domare #" + i + ": ");
+                double points = input.nextDouble();
+
+                min = Math.min(points, min);
+                max = Math.max(points, max);
+                sum += points;
+            }
+
+            sum = sum - max - min;
+            sum = sum / (ref-2);
+            sum = sum * 3 * level;
+
+            System.out.println("Poäng: " + sum);
+        }
     }
 }
