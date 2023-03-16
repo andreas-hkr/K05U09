1ee857604a0bed8c348b51a7ec9a596636be2048
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 07:20:10 +0100

    Fix comments

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
         // Beräkna simhoppspoäng
         //   - Ange antal domare (mist 3)
         //
-        //   - Läs in Svårighetsgrad
-        //   - Läs in poäng från alla domare
-        //   - Dra bort högsta och lägsta poängen
-        //   - Räkna ut genomsnitt av kvarvarande (obs 2 färre domare)
-        //   - Multiplicera med 3
-        //   - Multiplicera med svårighetsgraden
+        //   - Loop
+        //     - Läs in Svårighetsgrad
+        //     - Läs in poäng från alla domare
+        //     - Dra bort högsta och lägsta poängen
+        //     - Räkna ut genomsnitt av kvarvarande (obs 2 färre domare)
+        //     - Multiplicera med 3
+        //     - Multiplicera med svårighetsgraden
         //
         // Fortsätt tills svårighetsgraden är negativ
 
