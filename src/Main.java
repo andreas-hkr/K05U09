c8d5d1b2c94d810487c5ce82a1a94151de20ae98
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 07:16:56 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Beräkna simhoppspoäng
+        //   - Ange antal domare (mist 3)
+        //
+        //   - Läs in Svårighetsgrad
+        //   - Läs in poäng från alla domare
+        //   - Dra bort högsta och lägsta poängen
+        //   - Räkna ut genomsnitt av kvarvarande (obs 2 färre domare)
+        //   - Multiplicera med 3
+        //   - Multiplicera med svårighetsgraden
+        //
+        // Fortsätt tills svårighetsgraden är negativ
+
+
+    }
+}
